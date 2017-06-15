85fcd419c2bb374f470835ac26c90ec2eeaeb4d5
huangxg <huangxg@succez.com>
2017-06-15 18:52:52 +0800

    习题二与习题三的修改

--- test1/src/com/huang/test2/Testtwo.java
+++ test1/src/com/huang/test2/Testtwo.java
@@ -1,36 +1,46 @@
 package com.huang.test2;
+
 import java.util.Scanner;
 
 public class Testtwo {
-/*
- * 通过输入一个整数，将其转化为16进制数以字符串形式输出
- * @author huangxg@succez.com
- * @createdate 2017/6/14
- */
+	/**
+	 * 将输入的整数转化为16进制数以字符串形式输出
+	 * 
+	 * @author huangxg@succez.com
+	 * @createdate 2017/6/14
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner out = new Scanner(System.in);
-		int numInt=out.nextInt();
-		String result=intToHex(numInt);
+		int numInt = out.nextInt();
+		String result = intToHex(numInt);
 		System.out.println(result);
 	}
-   static String intToHex(int num){
-	   String[] charnum={"0","1","2","3","4","5","6","7"
-			   ,"8","9","A","B","C","D","E","F"};
-	   String hex1="";
-	   //在while循环前处理输入的数字为0，避免程序出错
-	   if(num==0)
-		   return "0";
-	   while(num!=0){
-		   hex1=hex1+charnum[num%16];
-		   num=num/16;
-	   }
-	   char[] hex2=new char[hex1.length()];
-	   char[] hex=new char[hex1.length()];
-	   hex2=hex1.toCharArray();
-	   for(int i=0;i<hex1.length();i++){
-		   hex[hex1.length()-1-i]=hex2[i];
-	   }
-	   return new String(hex);
-   }
+
+	/**
+	 * 
+	 * @param {num}
+	 *			num 原十进制数
+	 * @return
+	 */
+	static String intToHex(int num) {
+		String[] charnum = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F" };
+		String hex1 = "";
+		/**
+		 * 解决num为0时，while不循环导致程序不能输出的错误
+		 */
+		if (num == 0)
+			return "0";
+		while (num != 0) {
+			hex1 = hex1 + charnum[num % 16];
+			num = num / 16;
+		}
+		char[] hex = new char[hex1.length()];
+		char[] hex2 = new char[hex1.length()];
+		hex2 = hex1.toCharArray();
+		for (int i = 0; i < hex1.length(); i++) {
+			hex[hex1.length() - 1 - i] = hex2[i];
+		}
+		return new String(hex);
+	}
 }
--- test1/src/com/huang/test3/test3.java
+++ test1/src/com/huang/test3/test3.java
@@ -2,68 +2,86 @@ package com.huang.test3;
 
 import java.util.ArrayList;
 import java.util.Scanner;
-/*
+
+/**
  * 根据输入层次按顺序返回二叉树该层树节点
+ * 
  * @author huangxg@succez.com
  * @createdate 2017/6/14
  */
 public class test3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		//测试树
-		TNode tree8=new TNode("I",null,null);
-		TNode tree7=new TNode("H",null,null);
-		TNode tree6=new TNode("G",null,null);
-		TNode tree5=new TNode("F",null,null);
-		TNode tree4=new TNode("E",null,null);
-		TNode tree3=new TNode("D",tree7,tree8);
-		TNode tree2=new TNode("C",tree5,tree6);
-		TNode tree1=new TNode("B",tree3,tree4);
-		TNode tree=new TNode("A",tree1,tree2);
-		ArrayList<TNode> tArr=new ArrayList<TNode>();
+		TNode tree8 = new TNode("I", null, null);
+		TNode tree7 = new TNode("H", null, null);
+		TNode tree6 = new TNode("G", null, null);
+		TNode tree5 = new TNode("F", null, null);
+		TNode tree4 = new TNode("E", null, null);
+		TNode tree3 = new TNode("D", tree7, tree8);
+		TNode tree2 = new TNode("C", tree5, tree6);
+		TNode tree1 = new TNode("B", tree3, tree4);
+		TNode tree = new TNode("A", tree1, tree2);
+		ArrayList<TNode> tArr = new ArrayList<TNode>();
 		Scanner out = new Scanner(System.in);
-		int numInt=out.nextInt();
-		tArr=TreeLevel(tree,numInt);
-		for(int i=0;i<tArr.size();i++){
-			if(i<tArr.size()-1){
-				System.out.print(tArr.get(i).value+"-");
-			}else{
+		int numInt = out.nextInt();
+		tArr = TreeLevel(tree, numInt);
+		for (int i = 0; i < tArr.size(); i++) {
+			if (i < tArr.size() - 1) {
+				System.out.print(tArr.get(i).value + "-");
+			}
+			else {
 				System.out.print(tArr.get(i).value);
 			}
-				
+
 		}
 	}
-	static ArrayList<TNode> TreeLevel(TNode tree, int n){
-		ArrayList<TNode> nodeArr=new ArrayList<TNode>();
-		ArrayList<TNode> nodeArrS=new ArrayList<TNode>();
-		//当n=1，直接返回根节点
-		nodeArr.add(tree);
-		/*
-		 * 从树根节点循环去节点的左右子节点
+
+	/**
+	 * 
+	 * @param {tree}
+	 * 			tree 遍历树
+	 * @param {n}
+	 * 			n 树的第n层
+	 * @return
+	 */
+	static ArrayList<TNode> TreeLevel(TNode tree, int n) {
+		ArrayList<TNode> nodeArr = new ArrayList<TNode>();
+		ArrayList<TNode> nodeArrS = new ArrayList<TNode>();
+		/**
+		 * 当n=1，直接返回根节点
 		 */
-		for(int i=1;i<n;i++){
+		nodeArr.add(tree);
+		for (int i = 1; i < n; i++) {
 			//清除nodeArrS里面原有的节点
 			nodeArrS.clear();
 			nodeArrS.addAll(nodeArr);
 			nodeArr.clear();
-			for(int j=0;j<nodeArrS.size();j++){
-				if(nodeArrS.get(j).left!=null)
-				nodeArr.add(nodeArrS.get(j).left);
-				if(nodeArrS.get(j).right!=null)
-				nodeArr.add(nodeArrS.get(j).right);
+			/**
+			 * 从根向下查找left与right子节点，得到left与right子节点再进行下一层节点的查找
+			 */
+			for (int j = 0; j < nodeArrS.size(); j++) {
+				if (nodeArrS.get(j).left != null)
+					nodeArr.add(nodeArrS.get(j).left);
+				if (nodeArrS.get(j).right != null)
+					nodeArr.add(nodeArrS.get(j).right);
 			}
 		}
 		return nodeArr;
 	}
 }
-class TNode  {
-        String  value;
-        TNode   left,right;
-        TNode(){  }
-        TNode(String value,TNode left,TNode right){
-        	this.value=value;
-        	this.left=left;
-        	this.right=right;
-        }
-  }
+
+class TNode {
+	String value;
+
+	TNode left, right;
+
+	TNode() {
+	}
+
+	TNode(String value, TNode left, TNode right) {
+		this.value = value;
+		this.left = left;
+		this.right = right;
+	}
+}
