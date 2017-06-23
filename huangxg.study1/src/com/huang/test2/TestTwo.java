8d436fb62dfdb523b7f594b9ae62dfeee4b7c494
huangxg <huangxg@succez.com>
2017-06-23 09:34:51 +0800

    习题二的代码结构优化

--- huangxg.study1/src/com/huang/test2/TestTwo.java
+++ huangxg.study1/src/com/huang/test2/TestTwo.java
@@ -9,6 +9,9 @@ package com.huang.test2;
  * @createdate 2017年6月21日
  */
 public class TestTwo {
+	private static final char[] CHARNUM = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E',
+			'F' };
+
 	/**
 	 * 20170621 huangxg
 	 * 修改字符串的拼接带来的内存浪费问题
@@ -17,38 +20,32 @@ public class TestTwo {
 	 * @return num=0,直接返回"0"，num=Integer.MIN_VALUE,直接返回"-80000000"
 	 */
 	public String intToHex(int num) {
-		char[] charnum = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
-		char plusMinusFlag = ' ';
-		//取0和取1都可以的情况下，取与1可避免1位数的循环
-		int len = 1;
-		if (num < 0) {
-			/**
-			 * 解决最小int值-2147483648的绝对值超出int上限
-			 */
-			if (num == Integer.MIN_VALUE) {
-				return "-80000000";
-			}
-			len++;
-			plusMinusFlag = '-';
-			num = Math.abs(num);
+		/**
+		 * 解决最小int值-2147483648的绝对值超出int上限
+		 */
+		if (num == Integer.MIN_VALUE) {
+			return "-80000000";
 		}
 		/**
 		 * 解决num为0时，while不循环导致程序不能输出的错误
 		 */
 		if (num == 0)
 			return "0";
-		int num1 = num;
-		while (num1 / 16 != 0) {
-			num1 = num1 / 16;
+		char plusMinusFlag = ' ';
+		int len = 8;
+		if (num < 0) {
 			len++;
+			plusMinusFlag = '-';
+			num = Math.abs(num);
 		}
 		char[] hex = new char[len];
-		hex[0] = plusMinusFlag;
 		while (num != 0) {
-			len--;
-			hex[len] = charnum[num % 16];
-			num = num / 16;
+			hex[--len] = CHARNUM[num & 0xF];
+			num = num >>> 4;
 		}
-		return new String(hex);
+		if (plusMinusFlag == '-') {
+			hex[--len] = plusMinusFlag;
+		}
+		return new String(hex, len, plusMinusFlag == '-' ? 9 - len : 8 - len);
 	}
 }
