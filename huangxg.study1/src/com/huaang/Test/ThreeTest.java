a1e775b29121e8cc575a4d37114db7d980c6af03
huangxg <huangxg@succez.com>
2017-06-27 16:36:53 +0800

    习题三测试用来修改

--- huangxg.study1/src/com/huaang/Test/ThreeTest.java
+++ huangxg.study1/src/com/huaang/Test/ThreeTest.java
@@ -2,7 +2,8 @@ package com.huaang.Test;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.Random;
+import java.util.Objects;
+import java.util.Stack;
 
 import org.junit.After;
 import org.junit.Before;
@@ -15,8 +16,6 @@ public class ThreeTest {
 	private String[] nodesValueArr = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
 			"N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
 
-	StringBuilder[] sBuilders = new StringBuilder[26];
-
 	TestThree tThree = new TestThree();
 
 	@Before
@@ -27,62 +26,113 @@ public class ThreeTest {
 	public void tearDown() throws Exception {
 	}
 
-	//生成随机树
-	private TNode createTree() {
-		int nodes = new Random().nextInt(25) + 1;
-		TNode[] tNodeArr = new TNode[nodes];
-		//当前处理节点的索引值
-		int doNodeIndex = 0;
-		//当前分配节点的索引值
-		int giveNodeIndex = 1;
-		for (int i = 0; i < nodes; i++) {
-			tNodeArr[i] = new TNode(nodesValueArr[i], null, null);
+	private TNode createTree(String treeString) {
+		if (treeString == null || treeString == "") {
+			return null;
 		}
-		while (giveNodeIndex < nodes) {
-			if (new Random().nextBoolean()) {
-				tNodeArr[doNodeIndex].setLeft(tNodeArr[giveNodeIndex++]);
-			}
-			if (giveNodeIndex < nodes && (tNodeArr[doNodeIndex].getLeft() == null || new Random().nextBoolean())) {
-				tNodeArr[doNodeIndex++].setRight(tNodeArr[giveNodeIndex++]);
+		String[] delCharArr = treeString.split("");
+		int length = treeString.length();
+		int arrLength = 0;
+		//通过字符数求节点数
+		for (int j = 0; j < length; j++) {
+			char delChar = treeString.charAt(j);
+			if ((delChar >= 65 && delChar <= 90) || delChar == 32) {
+				arrLength++;
 			}
 		}
-		return tNodeArr[0];
+		TNode[] nodesArr = new TNode[arrLength];
+		for (int i = 0; i < arrLength; i++) {
+			nodesArr[i] = new TNode(nodesValueArr[i], null, null);
+		}
+		Stack<String> charArr = new Stack<String>();
+		for (int i = length - 1; i > 0; i--) {
+			if (!Objects.equals(delCharArr[i], "(")) {
+				charArr.push(delCharArr[i]);
+			}
+			else {
+				TNode node = getNode(nodesArr, delCharArr[i - 1]);
+				node.setLeft(getNode(nodesArr, charArr.pop()));
+				charArr.pop();
+				node.setRight(getNode(nodesArr, charArr.pop()));
+				charArr.pop();
+			}
+		}
+		return nodesArr[0];
 	}
 
-	String treeString(TNode tree, int n) {
-		if (n <= 0 || tree == null) {
+	TNode getNode(TNode[] nodesArr, String val) {
+		for (int i = 0, length = nodesArr.length; i < length; i++) {
+			if (Objects.equals(nodesArr[i].getValue(), val)) {
+				return nodesArr[i];
+			}
+		}
+		return null;
+	}
+
+	//创建节点层连接字符串
+	private String createTreeString(String treeString, int lvl) {
+		if (lvl <= 0 || treeString == null || treeString == "") {
 			return "";
 		}
-		StringBuffer str = new StringBuffer();
-		treeQuery(tree, n, str);
-		return str.toString();
+		int len = getStringArrLen(treeString);
+		if (lvl > len) {
+			return "";
+		}
+		StringBuilder[] lvlTreeArrString = new StringBuilder[len];
+		for (int i = 0; i < len; i++) {
+			lvlTreeArrString[i] = new StringBuilder();
+		}
+		int lvlIndex = 0;
+		for (int i = 0, length = treeString.length(); i < length; i++) {
+			char charFlag = treeString.charAt(i);
+			switch (charFlag) {
+				case '(':
+					lvlIndex++;
+					break;
+				case ')':
+					lvlIndex--;
+					break;
+				case ',':
+					break;
+				default:
+					if (!(charFlag == ' ')) {
+						lvlTreeArrString[lvlIndex].append(charFlag).append('-');
+					}
+					break;
+			}
+		}
+		StringBuilder retString = lvlTreeArrString[lvl - 1];
+		return retString.substring(0, retString.length() - 1);
 	}
 
-	public void treeQuery(TNode tree, int n, StringBuffer str) {
-		if (n <= 0 || tree == null) {
-			return;
+	//获取树层数
+	private int getStringArrLen(String treeString) {
+		if (treeString == null || treeString == "") {
+			return 0;
 		}
-		if (n == 1) {
-			if (str.length() > 0) {
-				str.append('-');
+		int len = 0;
+		for (int i = 0, length = treeString.length(); i < length; i++) {
+			if (treeString.charAt(i)=='(') {
+				len++;
 			}
-			str.append(tree.getValue());
 		}
-		treeQuery(tree.getLeft(), n - 1, str);
-		treeQuery(tree.getRight(), n - 1, str);
+		return (len / 2) + 1;
 	}
 
 	@Test
-	public void TreeTreeTest() {
-		TNode tree = createTree();
-		assertEquals(treeString(tree, -1), tThree.treeLevel(tree, -1));
-		assertEquals(treeString(tree, 1), tThree.treeLevel(tree, 1));
-		assertEquals(treeString(tree, 2), tThree.treeLevel(tree, 2));
-		assertEquals(treeString(tree, 3), tThree.treeLevel(tree, 3));
-		assertEquals(treeString(tree, 4), tThree.treeLevel(tree, 4));
-		assertEquals(treeString(tree, 5), tThree.treeLevel(tree, 5));
+	public void TreeTest() {
+		String[] treeStr = new String[] { "A(B(D(H, ),E(J, )),C(F( ,M),G( ,O)))", "", null,
+				"A(B(D(H,I),E(J,K)),C(F(L,M),G(N,O)))" };
+		for (int i = 0, length = treeStr.length; i < length; i++) {
+			TNode tree = createTree(treeStr[i]);
+			assertEquals(createTreeString(treeStr[i], -1), tThree.treeLevel(tree, -1));
+			assertEquals(createTreeString(treeStr[i], 1), tThree.treeLevel(tree, 1));
+			assertEquals(createTreeString(treeStr[i], 2), tThree.treeLevel(tree, 2));
+			assertEquals(createTreeString(treeStr[i], 3), tThree.treeLevel(tree, 3));
+			assertEquals(createTreeString(treeStr[i], 4), tThree.treeLevel(tree, 4));
+			assertEquals(createTreeString(treeStr[i], 5), tThree.treeLevel(tree, 5));
 	}
-
+	}
 	@Test
 	public void nullTree() {
 		assertEquals("", tThree.treeLevel(null, 1));
