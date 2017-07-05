da882e21ac19b7c52788c2dea6539421a6760c21
huangxg <huangxg@succez.com>
2017-07-05 16:28:30 +0800

    删除DelFileDao文件部分方法

--- haungxg.study2/src/com/huang/dao/DelFileDao.java
+++ haungxg.study2/src/com/huang/dao/DelFileDao.java
@@ -2,12 +2,9 @@ package com.huang.dao;
 
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import java.util.Arrays;
 import java.util.Objects;
 
 import com.huang.beans.DoInfoBean;
@@ -25,30 +22,6 @@ public class DelFileDao {
 	private static final String FAIL = "fail";
 
 	DoInfoBean dInfoBean = new DoInfoBean();
-
-	private static String[] fileSize = { "B", "KB", "M", "G", "T" };
-
-	//获取文件大小
-	private String getFileLength(File file) {
-		long size = file.length();
-		int index = 0;
-		double length = size;
-		while (length > 1024) {
-			length /= 1024;
-			index++;
-		}
-		String lenStr = Double.toString(length);
-		int pointAddress = lenStr.indexOf(".");
-		return lenStr.substring(0, pointAddress + 4) + fileSize[index];
-	}
-
-	private String getFileLastUpdateTime(File file) {
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		Calendar cal = Calendar.getInstance();
-		cal.setTimeInMillis(file.lastModified());
-		String modify = sdf.format(cal.getTime());
-		return modify;
-	}
 
 	/**
 	 * 在指定的目录中添加文件
@@ -169,52 +142,6 @@ public class DelFileDao {
 		return SUCCESS;
 	}
 
-	public String getFileContext(DoInfoBean info) {
-		if (!checkInfo(info)) {
-			return FAIL;
-		}
-		String filenameTemp = info.getPath() + info.getName();
-		File file = new File(filenameTemp);
-		String context = FAIL;
-		try {
-			context = new String(file2buf(file));
-		}
-		catch (FileNotFoundException e) {
-			e.printStackTrace();
-			return FAIL;
-		}
-		catch (NullPointerException e) {
-			e.printStackTrace();
-			return FAIL;
-		}
-		return context;
-	}
-
-	/**
-	 * 对DoInfoBean对象进行检测
-	 * <pre>
-	 *	 checkInfo(null)=false;
-	 * </pre>
-	 * @param info
-	 * @return false :如果参数对象的path属性与name属性有一个为空。
-	 */
-	private boolean checkInfo(DoInfoBean info) {
-		if (info == null) {
-			return false;
-		}
-		else if (Objects.equals(info.getPath(), null)) {
-			return false;
-		}
-		else if (Objects.equals(info.getName(), null)) {
-			return false;
-		}
-		return true;
-	}
-
-	public void downFile(DoInfoBean info) {
-
-	}
-
 	/**
 	 * 返回指定目录下所有的文件名和文件夹名
 	 * <pre>
@@ -224,84 +151,18 @@ public class DelFileDao {
 	 * @return
 	 */
 	public String getFolderList(DoInfoBean info) {
-		if (Objects.equals(info, null) || Objects.equals(info.getPath(), null)) {
+		if (Objects.equals(info, null)) {
 			return FAIL;
 		}
 		String filenameTemp = info.getPath();
-		File file = new File(filenameTemp);
+		System.out.println(filenameTemp);
+		File file = new File("D:/TestTwo/" + filenameTemp);
 		if (!file.exists()) {
 			return FAIL;
 		}
-		File fa[] = file.listFiles();
-		int length = fa.length;
-		if (length == 0) {
-			return FAIL;
-		}
+		String[] fa = file.list();
 		StringBuilder folderNameArr = new StringBuilder();
-		for (int i = 0; i < length; i++) {
-			if (fa[i] != null && fa[i].isFile() && !fa[i].isHidden()) {
-				folderNameArr.append(fa[i].getName()).append('|');
-				folderNameArr.append(getFileLength(fa[i])).append('|');
-				folderNameArr.append(getFileLastUpdateTime(fa[i])).append('|');
-				folderNameArr.append("true").append('*');
-			}
-			else if (!fa[i].isHidden()) {
-				folderNameArr.append(fa[i].getName()).append('|');
-				folderNameArr.append('\0').append('|');
-				folderNameArr.append(getFileLastUpdateTime(fa[i])).append('|');
-				folderNameArr.append("false").append('*');
-			}
-		}
-		int strLength = folderNameArr.length() - 1;
-		return new String(folderNameArr.substring(0, strLength));
-	}
-
-	/**
-	 * 用byte数组替换读取到的文件内容
-	 * 
-	 * @param file 读取的文件对象
-	 * @return 返回文件内容转换成的byte数组
-	 * @throws FileNotFoundException 如果文件不存在或者传入的是目录
-	 * @throws NullPointerException  如果传入的是null
-	 */
-	private byte[] file2buf(File file) throws FileNotFoundException, NullPointerException {
-		if (file == null)
-			throw new NullPointerException();
-		if (!file.exists() || file.isDirectory())
-			throw new FileNotFoundException();
-		long fileSize = file.length();
-		if (fileSize > Integer.MAX_VALUE) {
-			System.out.println("file too big...");
-			return null;
-		}
-		int offset = 0;
-		int numRead = 0;
-		FileInputStream fi = null;
-		byte[] buffer = null;
-		try {
-			fi = new FileInputStream(file);
-			buffer = new byte[(int) fileSize];
-			int length = buffer.length;
-			while (offset < length
-					&& (numRead = fi.read(buffer, offset, length - offset > 4096 ? 4096 : length - offset)) >= 0) {
-				offset += numRead;
-			}
-			if (offset != length) {
-				throw new IOException("Could not completely read file " + file.getName());
-			}
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-		}
-		finally {
-			try {
-				if (fi != null) {
-					fi.close();
-				}
-			}
-			catch (IOException e) {
-			}
-		}
-		return buffer;
+		folderNameArr.append(Arrays.toString(fa));
+		return new String(folderNameArr);
 	}
 }
